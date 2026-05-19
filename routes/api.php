72a7b06444438e21c89aae6df59465487f86b898
Yassine Afi <yassineafi31@gmail.com>
2026-05-19 01:14:10 +0100

    Remove temp routes security

--- routes/api.php
+++ routes/api.php
@@ -4,23 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AdminAuthController;
 use App\Http\Controllers\Admin\DashboardController;
-use Illuminate\Support\Facades\Artisan;
-
-Route::get('/create-admin', function () {
-    try {
-        \App\Models\User::updateOrCreate(
-            ['email' => 'admin@nexolabs.ma'],
-            [
-                'name' => 'Admin NexoLabs',
-                'password' => \Illuminate\Support\Facades\Hash::make('admin1234'),
-            ]
-        );
-        return response()->json(['message' => 'Admin créé !']);
-    } catch (\Exception $e) {
-        return response()->json(['error' => $e->getMessage()], 500);
-    }
-});
-
 
 // Public
 Route::post('/contact', [ContactController::class, 'store']);
