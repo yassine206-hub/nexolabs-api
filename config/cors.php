c2ece081324ffbfec9ac1fb83fc59ed4bda447b1
Yassine Afi <yassineafi31@gmail.com>
2026-05-19 01:04:36 +0100

    Fix CORS for Vercel

--- config/cors.php
+++ config/cors.php
@@ -2,18 +2,14 @@
 
 return [
     'paths' => ['api/*'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:5173'],
-
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://nexolabs-frontend.vercel.app',
+    ],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
 ];
